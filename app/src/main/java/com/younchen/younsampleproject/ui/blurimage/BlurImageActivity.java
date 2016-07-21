fb31526783d54734715e748281b3aa11cf7871af
龙泉 <mryoun@163.com>
2016-07-21 18:19:25 +0800

    test upload to github

---
+++ app/src/main/java/com/younchen/younsampleproject/ui/blurimage/BlurImageActivity.java
@@ -0,0 +1,15 @@
+package com.younchen.younsampleproject.ui.blurimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.younchen.younsampleproject.R;
+
+public class BlurImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blur_image);
+    }
+}
